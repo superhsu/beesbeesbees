9df4abaeaa8868ae629100ca5bec8880f51ae2de
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-22 12:25:00 -0800

    complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this);
+  Bee.prototype.constructor = Bee;
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+
 
